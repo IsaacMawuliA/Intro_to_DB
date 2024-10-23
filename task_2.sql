814a27e37f235fb4df0ba03ce51b9004a8f01356
IsaacMawuliA <m.want35@gmail.com>
2024-10-23 02:18:04 -0400

    Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -27,11 +27,11 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE OrderDetails (
+CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
