3fc06d677c21528d9ed2157b3e55179e19706600
IsaacMawuliA <m.want35@gmail.com>
2024-10-23 02:59:04 -0400

    Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,4 @@
 INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-
-INSERT INTO Customers (customer_name, email, address) VALUES
-('New Customer', 'newcustomer@example.com', '124 Happiness Ave.');
-
-UPDATE Customers
-SET address = '124 Happiness Ave.'
-WHERE customer_id = [specific_id];
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.')As new_values
+ON DUPLICATE KEY UPDATE
+customer_name = new_values.customer_name, email = new_values.email, address = new_values.address;
