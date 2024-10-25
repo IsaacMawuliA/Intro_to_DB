a6de244635f3df5d2fc3a0ab2bcd4268ba1d96bf
IsaacMawuliA <m.want35@gmail.com>
2024-10-24 23:54:14 -0400

    Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,20 +1,6 @@
-INSERT INTO customer
-( customer_id = 2,
-customer_name = Blessing Malik,
-email = bmalik@sandtech.com,
-address = 124 Happiness Ave,
+USE alx_book_store;
 
-customer_id = 3,
-customer_name = Obed Ehoneah,
-email = eobed@sandtech.com,
-address = 125 Happiness  Ave,
-
-customer_id = 4,
-customer_name = Nehemial Kamolu,
-email = nkamolu@sandtech.com,
-address = 126 Happiness  Ave,
-)
-
-As new_values
-ON DUPLICATE KEY UPDATE
-customer_name = new_values.customer_name, email = new_values.email, address = new_values.address;
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
